aac9a6efa00110c39a483523eb5612a023c9bd9b
enriquetorres2 <enrique.torres2@upr.edu>
2016-10-26 18:59:03 -0400

    BluetoothModuleV1

---
+++ BluetoothModule/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5529.cmd 
+
+C_SRCS += \
+../SerialBluetooth.c \
+../testBT.c 
+
+C_DEPS += \
+./SerialBluetooth.d \
+./testBT.d 
+
+OBJS += \
+./SerialBluetooth.obj \
+./testBT.obj 
+
+OBJS__QUOTED += \
+"SerialBluetooth.obj" \
+"testBT.obj" 
+
+C_DEPS__QUOTED += \
+"SerialBluetooth.d" \
+"testBT.d" 
+
+C_SRCS__QUOTED += \
+"../SerialBluetooth.c" \
+"../testBT.c" 
+
+
---
+++ BluetoothModule/Debug/testBT.d
@@ -0,0 +1,17 @@
+# FIXED
+
+testBT.obj: ../testBT.c
+testBT.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/msp430.h
+testBT.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+testBT.obj: /Applications/ti/ccsv6/ccs_base/msp430/include/in430.h
+testBT.obj: /Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h
+testBT.obj: /Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h
+testBT.obj: ../SerialBluetooth.h
+
+../testBT.c: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+/Applications/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+/Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics.h: 
+/Applications/ti/ccsv6/tools/compiler/msp430_15.12.3.LTS/include/intrinsics_legacy_undefs.h: 
+../SerialBluetooth.h: 
