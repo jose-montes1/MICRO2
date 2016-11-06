9ef2f153c77ead327710333938250376dbb1fce4
jose-montes1 <jose.montes1@upr.edu>
2016-11-05 21:27:20 -0400

    Eagle libs and DMP files

---
+++ eMD-6.0/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -l"//fs.ece.uprm.edu/students/jose.montes1/My\ Documents/MICRO2/eMD-6.0/core/mpl/libmplmpu.lib" -l"libc.a"
+
